67ab9c15ed071552ff0d7268177cab4a7adc6338
Dovzenko Yaroslav <yrik9890@gmail.com>
2024-02-22 10:08:59 +0300

    consolprilo

---
+++ povtoreniya-n-.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace povtoreniya_n_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите количевство повторений (n):");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сообщение для повторения:");
+            string message = Console.ReadLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}.{message} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
